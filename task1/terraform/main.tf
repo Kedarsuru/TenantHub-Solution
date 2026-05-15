65a9b578befb17bfcbb4cae96f46a5496ea40d6c
Kedarsuru <142992460+Kedarsuru@users.noreply.github.com>
2026-05-15 15:15:42 +0530

    Update main.tf

--- task1/terraform/main.tf
+++ task1/terraform/main.tf
@@ -1,1 +1,10 @@
+module "tenant_databases" {
+  source = "./modules/tenant-db"
 
+  for_each = var.tenants
+
+  tenant_name            = each.key
+  db_name                = each.value.db_name
+  db_user                = each.value.db_user
+  cloudsql_instance_name = var.cloudsql_instance_name
+}
