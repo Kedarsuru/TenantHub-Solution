59004f6989a902d769341a2450023985c1cf5ad7
Kedarsuru <142992460+Kedarsuru@users.noreply.github.com>
2026-05-15 15:23:51 +0530

    Update secret-manager.tf

--- Task2/terraform/secret-manager.tf
+++ Task2/terraform/secret-manager.tf
@@ -1,1 +1,17 @@
+resource "google_secret_manager_secret" "tenant_secret" {
+  secret_id = "tenant-acme-corp-credentials"
 
+  replication {
+    auto {}
+  }
+}
+
+resource "google_secret_manager_secret_version" "tenant_secret_version" {
+  secret = google_secret_manager_secret.tenant_secret.id
+
+  secret_data = jsonencode({
+    username = "acme_corp_user"
+    password = "super-secret-password"
+    database = "acme_corp_db"
+  })
+}
