8d3a6b508a908aa82304d22dc7c8bce1955f90bc
Kedarsuru <142992460+Kedarsuru@users.noreply.github.com>
2026-05-15 15:16:59 +0530

    Update main.tf

--- task1/terraform/modules/tenant-db/main.tf
+++ task1/terraform/modules/tenant-db/main.tf
@@ -1,1 +1,15 @@
+resource "random_password" "db_password" {
+  length  = 20
+  special = true
+}
 
+resource "google_sql_database" "tenant_database" {
+  name     = var.db_name
+  instance = var.cloudsql_instance_name
+}
+
+resource "google_sql_user" "tenant_user" {
+  name     = var.db_user
+  instance = var.cloudsql_instance_name
+  password = random_password.db_password.result
+}
