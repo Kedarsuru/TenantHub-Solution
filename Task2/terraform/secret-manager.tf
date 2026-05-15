b72c2129c127bb00789931cdb3d6e675d510e5f7
Kedarsuru <142992460+Kedarsuru@users.noreply.github.com>
2026-05-15 15:38:05 +0530

    Update secret-manager.tf

--- Task2/terraform/secret-manager.tf
+++ Task2/terraform/secret-manager.tf
@@ -15,3 +15,23 @@ resource "google_secret_manager_secret_version" "tenant_secret_version" {
     database = "acme_corp_db"
   })
 }
+
+resource "google_service_account" "tenant_gsa" {
+  account_id   = "acme-corp-gsa"
+  display_name = "GSA for acme-corp tenant"
+}
+
+resource "google_secret_manager_secret_iam_member" "secret_accessor" {
+  secret_id = google_secret_manager_secret.tenant_secret.id
+  role      = "roles/secretmanager.secretAccessor"
+
+  member = "serviceAccount:${google_service_account.tenant_gsa.email}"
+}
+
+resource "google_service_account_iam_member" "workload_identity_binding" {
+  service_account_id = google_service_account.tenant_gsa.name
+
+  role = "roles/iam.workloadIdentityUser"
+
+  member = "serviceAccount:PROJECT_ID.svc.id.goog[acme-corp/acme-corp-sa]"
+}
