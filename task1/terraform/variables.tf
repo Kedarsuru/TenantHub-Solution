3e52d9a54311d91a66aa054f5e3e20a90ca400d7
Kedarsuru <142992460+Kedarsuru@users.noreply.github.com>
2026-05-15 15:15:16 +0530

    Update variables.tf

--- task1/terraform/variables.tf
+++ task1/terraform/variables.tf
@@ -1,1 +1,10 @@
+variable "project_id" {}
+variable "region" {}
+variable "cloudsql_instance_name" {}
 
+variable "tenants" {
+  type = map(object({
+    db_name = string
+    db_user = string
+  }))
+}
